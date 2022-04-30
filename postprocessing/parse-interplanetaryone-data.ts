7aa9009bd1983072f39fe9ec2ce66f894d70b86d
Alexandre Nicastro <alexandre.nicastro@nikas.org>
2022-04-30 12:59:46 -0300

    feat: add initial files and configs
    Signed-off-by: Alexandre Nicastro <alexandre.nicastro@nikas.org>

---
+++ postprocessing/parse-interplanetaryone-data.ts
@@ -0,0 +1,9 @@
+import { readJSON, writeJSON } from 'https://deno.land/x/flat@0.0.15/mod.ts';
+
+const inputFilename = Deno.args[0];
+const outputFilename = inputFilename;
+
+// let contents;
+// let parsedContents;
+
+// await writeJSON(outputFilename, parsedContents);
