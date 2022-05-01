3b1e1daa7f96205d03c9640e88f4b4eb64417431
Alexandre Nicastro <alexandre.nicastro@nikas.org>
2022-05-01 17:29:08 -0300

    *: add filplus-issues data & rename interplanetaryone data file
    Signed-off-by: Alexandre Nicastro <alexandre.nicastro@nikas.org>

--- postprocessing/parse-interplanetaryone-data.ts
+++ postprocessing/parse-interplanetaryone-data.ts
@@ -1,7 +1,9 @@
-import { readJSON, writeJSON } from 'https://deno.land/x/flat@0.0.15/mod.ts';
+import { readJSON, writeJSON } from "https://deno.land/x/flat@0.0.15/mod.ts";
 
 const inputFilename = Deno.args[0];
-const outputFilename = inputFilename;
+const outputFilename = `${inputFilename}-processed`;
+
+const data = await readJSON(inputFilename);
 
 // let contents;
 // let parsedContents;
