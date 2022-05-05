0334e79dcba86dff1a19b8ef17e7a873cceb0882
Alexandre Nicastro <alexandre.nicastro@nikas.org>
2022-05-04 22:52:00 -0300

    fix(postprocessing): remove unused import and function
    Signed-off-by: Alexandre Nicastro <alexandre.nicastro@nikas.org>

--- postprocessing/parse-interplanetaryone-data.ts
+++ postprocessing/parse-interplanetaryone-data.ts
@@ -1,12 +1,9 @@
 import { readJSON, writeJSON } from "https://deno.land/x/flat@0.0.15/mod.ts";
-import { getAllIssues } from "../utils/github.ts";
 
 const inputFilename = Deno.args[0];
 const outputFilename = `${inputFilename}-processed`;
 
 // const data = await readJSON(inputFilename);
-
-console.log(await getAllIssues());
 
 // let contents;
 // let parsedContents;
