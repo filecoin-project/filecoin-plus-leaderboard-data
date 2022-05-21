677f5208d99dfc1b98d501f7674b96f26acf365b
Alexandre Nicastro <alexandre.nicastro@nikas.org>
2022-05-20 22:40:33 -0300

    feat(postprocessing): add concurrency control (throttle) to address mapping
    Signed-off-by: Alexandre Nicastro <alexandre.nicastro@nikas.org>

--- postprocessing/generate-address-mapping.ts
+++ postprocessing/generate-address-mapping.ts
@@ -1,5 +1,13 @@
 // deno-lint-ignore-file no-unused-vars no-explicit-any
-import { readJSON, writeJSON } from "https://deno.land/x/flat@0.0.15/mod.ts";
+import { readJSON, writeJSON } from 'https://deno.land/x/flat@0.0.15/mod.ts';
+import PQueue from 'https://cdn.skypack.dev/p-queue?dts';
+
+const queue = new PQueue({ concurrency: 4, interval: 500 });
+
+// let count = 0;
+// queue.on('active', () => {
+//   console.log(`Working on item #${++count}.  Size: ${queue.size}  Pending: ${queue.pending}`);
+// });
 
 type AddressMap = {
   addressId: string;
@@ -7,23 +15,23 @@ type AddressMap = {
 };
 
 const addressMap: AddressMap[] = [];
-console.log("addressMap ->", addressMap);
+console.log('addressMap ->', addressMap);
 
 const testAddressMap: AddressMap[] = [
-  { addressId: "", addressKey: "" },
-  { addressId: "f0107408", addressKey: "" },
-  { addressId: "", addressKey: "f1k6wwevxvp466ybil7y2scqlhtnrz5atjkkyvm4a" },
+  { addressId: '', addressKey: '' },
+  { addressId: 'f0107408', addressKey: '' },
+  { addressId: '', addressKey: 'f1k6wwevxvp466ybil7y2scqlhtnrz5atjkkyvm4a' },
   {
-    addressId: "f0107408",
-    addressKey: "f1k6wwevxvp466ybil7y2scqlhtnrz5atjkkyvm4a",
+    addressId: 'f0107408',
+    addressKey: 'f1k6wwevxvp466ybil7y2scqlhtnrz5atjkkyvm4a',
   },
 ];
 
 const notaryGovernanceIssues = await readJSON(
-  "./data/processed/notary-governance-issues.json",
+  './data/processed/notary-governance-issues.json',
 );
 const verifiersFromInterplanetaryOne = await readJSON(
-  "./data/raw/interplanetaryone-verifiers.json",
+  './data/raw/interplanetaryone-verifiers.json',
 );
 
 // TODO(alexxnica): change to on-chain checks or modularize to accept different lists
@@ -39,15 +47,11 @@ const getInitialAddresses = (
     addressKey = verifier.addressKey;
 
     if (!addressId && !!addressKey) {
-      addressId = verifiersFromInterplanetaryOne.find((v) =>
-        v.address === addressKey
-      )?.addressId;
+      addressId = verifiersFromInterplanetaryOne.find((v) => v.address === addressKey)?.addressId;
     }
 
     if (!addressKey && !!addressId) {
-      addressKey = verifiersFromInterplanetaryOne.find((v) =>
-        v.addressId === addressId
-      )?.address;
+      addressKey = verifiersFromInterplanetaryOne.find((v) => v.addressId === addressId)?.address;
     }
 
     return {
@@ -57,56 +61,68 @@ const getInitialAddresses = (
   });
 };
 
-const resolveAddressesWithGlif = (addressMap: AddressMap[]) => {
-  const apiEndpoint = "https://api.node.glif.io/rpc/v0";
+const resolveAddressesWithGlif = async (addressMap: AddressMap[]) => {
+  const newAddressMap: AddressMap[] = [];
+  const apiEndpoint = 'https://api.node.glif.io/rpc/v0';
 
-  return addressMap.map(async (addresses) => {
+  addressMap.forEach(async (addresses) => {
     let addressId, addressKey;
 
     addressId = addresses.addressId;
     addressKey = addresses.addressKey;
+    console.log(addresses);
 
     if (!addressId && !!addressKey) {
-      const getAddress = await fetch(`${apiEndpoint}`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          jsonrpc: "2.0",
-          method: "Filecoin.StateLookupID",
-          id: 1,
-          params: [`${addressKey}`, null],
-        }),
-      });
+      const getAddress = await queue.add(async () =>
+        await fetch(`${apiEndpoint}`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            jsonrpc: '2.0',
+            method: 'Filecoin.StateLookupID',
+            id: 1,
+            params: [`${addressKey}`, null],
+          }),
+        })
+      );
       const getAddressData = await getAddress.json();
+      // console.log('getAddressData ->', getAddressData);
       // console.log("addresses ->", addresses);
       addressId = getAddressData?.result;
       // console.log("Getting addressId ->", addressId);
     }
 
     if (!addressKey && !!addressId) {
-      const getAddress = await fetch(`${apiEndpoint}`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          jsonrpc: "2.0",
-          method: "Filecoin.StateAccountKey",
-          id: 1,
-          params: [`${addressId}`, null],
-        }),
-      });
+      const getAddress = await queue.add(async () =>
+        await fetch(`${apiEndpoint}`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            jsonrpc: '2.0',
+            method: 'Filecoin.StateAccountKey',
+            id: 1,
+            params: [`${addressId}`, null],
+          }),
+        })
+      );
       const getAddressData = await getAddress.json();
       // console.log("addresses ->", addresses);
       addressKey = getAddressData?.result;
       // console.log("Getting addressKey ->", addressKey);
     }
 
+    // console.log({ addressId: addressId || null, addressKey: addressKey || null });
     // console.log();
-    return { addressId: addressId || null, addressKey: addressKey || null };
+
+    newAddressMap.push({ addressId: addressId || null, addressKey: addressKey || null });
   });
+
+  await queue.onIdle();
+  return newAddressMap;
 };
 
 const verifiers = Object.create({});
@@ -117,15 +133,16 @@ verifiers.resolvedAddresses = getInitialAddresses(
   verifiers.fromInterplanetaryOne,
 );
 // verifiers.resolvedAddresses = testAddressMap;
-verifiers.resolvedAddresses = await Promise.all(resolveAddressesWithGlif(
+verifiers.resolvedAddresses = await resolveAddressesWithGlif(
   verifiers.resolvedAddresses,
-));
+);
+// console.log('verifiers.resolvedAddresses ->', verifiers.resolvedAddresses);
 
 // const buildAddressMap = (resolvedAddresses) => {};
 
 // console.log("verifiers.resolvedAddresses ->", verifiers.resolvedAddresses);
 
 await writeJSON(
-  "./data/generated/address-mapping.json",
+  './data/generated/address-mapping.json',
   verifiers.resolvedAddresses,
 );
