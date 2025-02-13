ada8187cf2e9c8058b48b148b168bc65d761f166
Alexandre Nicastro <alexandre.nicastro@nikas.org>
2025-02-13 17:53:11 -0300

    test: add unit tests for sanitizeString, isAddressKey, and isAddressId functions
    Signed-off-by: Alexandre Nicastro <alexandre.nicastro@nikas.org>

---
+++ tests/general_test.ts
@@ -0,0 +1,61 @@
+import {
+  sanitizeString,
+  isAddressKey,
+  isAddressId,
+} from '../utils/general.ts';
+import { assertEquals, assert } from "jsr:@std/assert";
+
+/**
+ * Tests for sanitizeString function.
+ */
+Deno.test("sanitizeString handles empty input", () => {
+  assertEquals(sanitizeString(""), "");
+});
+
+Deno.test("sanitizeString removes HTML tags", () => {
+  const input = "<div>Hello</div>";
+  const expected = "Hello";
+  assertEquals(sanitizeString(input), expected);
+});
+
+Deno.test("sanitizeString trims whitespace", () => {
+  const input = "  Hello  ";
+  const expected = "Hello";
+  assertEquals(sanitizeString(input), expected);
+});
+
+Deno.test("sanitizeString handles complex HTML", () => {
+  const input = "<div><p>Hello <span>World</span></p></div>";
+  const expected = "Hello World";
+  assertEquals(sanitizeString(input), expected);
+});
+
+/**
+ * Tests for isAddressKey function.
+ */
+Deno.test("isAddressKey validates address length", () => {
+  assert(isAddressKey("f12345678901234"));
+});
+
+Deno.test("isAddressKey invalidates short address", () => {
+  assert(!isAddressKey("f1234"));
+});
+
+Deno.test("isAddressKey invalidates long address", () => {
+  assert(!isAddressKey("f12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890"));
+});
+
+/**
+ * Tests for isAddressId function.
+ */
+Deno.test("isAddressId validates address length", () => {
+  assert(isAddressId("f1234"));
+});
+
+Deno.test("isAddressId invalidates short address", () => {
+  assert(!isAddressId("f1"));
+});
+
+Deno.test("isAddressId invalidates long address", () => {
+  assert(!isAddressId("f12345678901234567890"));
+});
